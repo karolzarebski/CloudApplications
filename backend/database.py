efff028f374504180aa4f8928dc0d29c4a9d8d72
Karol Zarębski <karolkarolski25@gmail.com>
2023-03-08 18:28:32 +0100

    db fix

--- backend/database.py
+++ backend/database.py
@@ -3,16 +3,14 @@ import mysql.connector
 
 class DatabaseService:
     def __init__(self, database_configuration):
-        self.db = None
-        self.conf = database_configuration
+        self.database_configuration = database_configuration
         self.create_database()
 
-    def __del__(self):
-        self.db.close()
-
     def create_database(self):
-        connection = mysql.connector.connect(**self.conf)
-        connection.cursor().execute('''
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor()
+
+        db_cursor.execute('''
             CREATE TABLE IF NOT EXISTS VideoData (
               Id INT PRIMARY KEY AUTO_INCREMENT,
               Title VARCHAR(255) NOT NULL,
@@ -24,69 +22,100 @@ class DatabaseService:
               Views DOUBLE
             );
         ''')
-        connection.cursor().close()
+
+        db_cursor.close()
         connection.close()
 
     def add_data(self, data):
-        db_cursor = self.db.cursor(dictionary=True, buffered=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
+
         db_cursor.execute('''INSERT INTO VideoData (Title, VideoId, PublishedAt, Keyword, Likes, Comments, Views) 
             VALUES (%s,%s,%s,%s,%s,%s,%s)''', (data['Title'], data['VideoId'], data['PublishedAt'], data['Keyword'],
                                                data['Likes'] if not data['Likes'] == '' else None,
                                                data['Comments'] if not data['Comments'] == '' else None,
                                                data['Views'] if not data['Views'] == '' else None))
-        self.db.commit()
+        connection.commit()
+
         db_cursor.execute('''SELECT * FROM VideoData WHERE Id = LAST_INSERT_ID()''')
         latest_entry = db_cursor.fetchone()
+
         db_cursor.close()
+        connection.close()
+
         return latest_entry
 
     def get_all_data(self):
-        connection = mysql.connector.connect(**self.conf)
-        cursor = connection.cursor(buffered=True, dictionary=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
 
-        cursor.execute('''SELECT * FROM VideoData''')
-        data = cursor.fetchall()
-        cursor.close()
+        db_cursor.execute('''SELECT * FROM VideoData''')
+        data = db_cursor.fetchall()
+
+        db_cursor.close()
         connection.close()
+
         return data
 
     def get_by_id(self, video_id):
-        db_cursor = self.db.cursor(buffered=True, dictionary=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
+
         db_cursor.execute('''SELECT * FROM VideoData WHERE Id = %s''', (video_id,))
         data = db_cursor.fetchone()
+
         db_cursor.close()
+        connection.close()
+
         return data
 
     def get_by_title(self, title):
-        db_cursor = self.db.cursor(buffered=True, dictionary=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
+
         db_cursor.execute('''SELECT * FROM VideoData WHERE Title LIKE %s''', (f"%{title.replace(',', ' ')}%",))
         data = db_cursor.fetchall()
+
         db_cursor.close()
+        connection.close()
+
         return data
 
     def get_by_keyword(self, keyword):
-        db_cursor = self.db.cursor(buffered=True, dictionary=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
+
         db_cursor.execute('''SELECT * FROM VideoData WHERE Keyword = %s''', (keyword,))
         data = db_cursor.fetchall()
+
         db_cursor.close()
+        connection.close()
+
         return data
 
     def update_video(self, data):
-        db_cursor = self.db.cursor(buffered=True, dictionary=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
+
         db_cursor.execute('''UPDATE VideoData SET Title = %s, VideoId = %s, PublishedAt = %s, Keyword = %s, Likes = %s,
                             Comments = %s, Views = %s  WHERE Id = %s''', (data['Title'], data['VideoId'],
                                                                           data['PublishedAt'], data['Keyword'],
                                                                           data['Likes'], data['Comments'],
                                                                           data['Views'], data['Id']))
-        self.db.commit()
+        connection.commit()
         db_cursor.close()
+        connection.close()
+
         return self.get_by_id(data['Id'])
 
     def get_keywords(self):
-        connection = mysql.connector.connect(**self.conf)
-        db_cursor = connection.cursor(buffered=True, dictionary=True)
+        connection = mysql.connector.connect(**self.database_configuration)
+        db_cursor = connection.cursor(dictionary=True, buffered=True)
+
         db_cursor.execute('''SELECT DISTINCT Keyword FROM VideoData''')
         data = db_cursor.fetchall()
+
         db_cursor.close()
         connection.close()
+        
         return data
