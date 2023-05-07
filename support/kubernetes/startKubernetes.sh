79918b6530484bb8b25ff5a844a0c5d16105228b
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2023-05-07 15:07:12 +0200

    Added wait time to start kubernetes scirpt so that db has time to setup

--- support/kubernetes/startKubernetes.sh
+++ support/kubernetes/startKubernetes.sh
@@ -3,6 +3,8 @@ kind create cluster --config=kind-cluster.yaml
 kind load docker-image production_frontend
 kind load docker-image production_backend
 kind load docker-image mysql:8.0.29
-kubectl apply -f database.yaml
-kubectl apply -f back.yaml
 kubectl apply -f front.yaml
+kubectl apply -f database.yaml
+echo Waiting 60 seconds for db to set up...
+ping -n 60 127.0.0.1 >nul
+kubectl apply -f back.yaml
